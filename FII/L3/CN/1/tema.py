724846fd0600eb28da62d12e214f0f029f1f5e9e
Cosmin Poieana <cmin764@gmail.com>
2017-03-08 15:42:40 +0200

    [FII] CN week3 sync

--- FII/L3/CN/1/tema.py
+++ FII/L3/CN/1/tema.py
@@ -5,8 +5,6 @@ import math
 import random
 import time
 
-import numpy as np
-
 
 C1 = 1.0 / math.factorial(3)
 C2 = 1.0 / math.factorial(5)
@@ -14,6 +12,19 @@ C3 = 1.0 / math.factorial(7)
 C4 = 1.0 / math.factorial(9)
 C5 = 1.0 / math.factorial(11)
 C6 = 1.0 / math.factorial(13)
+
+p1 = lambda x: x - C1 * x ** 3 + C2 * x ** 5
+p2 = lambda x: x - C1 * x ** 3 + C2 * x ** 5 - C3 * x ** 7
+p3 = lambda x: x - C1 * x ** 3 + C2 * x ** 5 - C3 * x ** 7 + C4 * x ** 9
+p4 = (lambda x: x - 0.166 * x ** 3 + 0.00833 * x ** 5 -
+        C3 * x ** 7 + C4 * x ** 9)
+p5 = (lambda x: x - C1 * x ** 3 + C2 * x ** 5 -
+        C3 * x ** 7 + C4 * x ** 9 - C5 * x ** 11)
+p6 = (lambda x: x - C1 * x ** 3 + C2 * x ** 5 -
+        C3 * x ** 7 + C4 * x ** 9 - C5 * x ** 11 + C6 * x ** 13)
+ps = [p1, p2, p3, p4, p5, p6]
+for idx, p in enumerate(ps):
+    setattr(p, "__name__", "p{}".format(idx + 1))
 
 def P1(x):
     x2 = x ** 2
@@ -39,7 +50,7 @@ def P6(x):
 def prob1():
     nr = 1/10.0
     count = 0
-    while nr / 10.0 != 0:
+    while (nr / 10.0) + 1.0 != 1.0:
         nr /= 10.0
         count += 1
     print("Numarul {} dupa {} iteratii.".format(nr, count))
@@ -52,21 +63,19 @@ def prob2(nr):
     y = z = nr
     e1 = (x + y) + z
     e2 = x + (y + z)
-    print(e1, e2, nr)
     print(e1 != e2)    # eroare corectata de interpretor
     # Asociativitatea inmultirii.
-    y = z = np.float(nr)
-    x = np.float(10) ** 300
+    x = random.uniform(0.9, 1.0)
+    y = z = nr
     e1 = (x * y) * z
     e2 = x * (y * z)
     print(e1 != e2)
 
 
-def prob3():
+def prob3(polys):
     count = 10000
     hi = math.pi / 2
     lo = -hi
-    polys = [P1, P2, P3, P4, P5, P6]
     errs = [0.0] * len(polys)
     for _ in range(count):
         nr = random.uniform(lo, hi)
@@ -80,11 +89,10 @@ def prob3():
         print("{} - {}".format(poly.__name__, final[poly]))
 
 
-def prob4():
+def prob4(polys):
     count = 100000
     hi = math.pi / 2
     lo = -hi
-    polys = [P1, P2, P3, P4, P5, P6]
     times = [0.0] * len(polys)
     for _ in range(count):
         nr = random.uniform(lo, hi)
@@ -106,12 +114,22 @@ def main():
     prob2(nr)
     print("")
 
+    polys = [p1, p2, p3, p4, p5, p6]
+    polys_simplified = [P1, P2, P3, P4, P5, P6]
     print("Problema 3 - ranking polinoame")
-    prob3()
+    prob3(polys)
+    print("")
+    print("Problema 3 - ranking polinoame (simplificat)")
+    prob3(polys_simplified)
     print("")
 
+    polys = [p1, p2, p3, p4, p5, p6]
+    polys_simplified = [P1, P2, P3, P4, P5, P6]
     print("Problema 4 - timp polinoame")
-    prob4()
+    prob4(polys)
+    print("")
+    print("Problema 4 - timp polinoame (simplificat)")
+    prob4(polys_simplified)
     print("")
 
 
