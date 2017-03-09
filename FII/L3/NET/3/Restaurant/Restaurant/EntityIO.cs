14bf2e2d6918234d6634da008424c3c9842a64b8
Cosmin Poieana <cmin764@gmail.com>
2017-03-09 15:11:30 +0200

    [FII] NET week3 sync

---
+++ FII/L3/NET/3/Restaurant/Restaurant/EntityIO.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+
+namespace Restaurant
+{
+    class EntityIO<T> where T : class
+    {
+        private static RestaurantContext db;
+        private readonly DbSet<T> dbset;
+
+        public EntityIO()
+        {
+            if (db == null)
+            {
+                db = new RestaurantContext();
+            }
+            dbset = db.Set<T>();
+        }
+
+        public DbSet<T> Dbset { get { return dbset; } }
+
+        public void Insert(T entity)
+        {
+            dbset.Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            dbset.Remove(entity);
+            db.SaveChanges();
+        }
+
+        public List<T> Retrieve(IOrderedQueryable<T> query)
+        {
+            return query.ToList();
+        }
+
+        public int Count()
+        {
+            return dbset.Count();
+        }
+
+        public void Clear()
+        {
+            foreach (var entity in dbset)
+            {
+                dbset.Remove(entity);
+            }
+        }
+    }
+}
---
+++ FII/L3/NET/3/Restaurant/Restaurant/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Restaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerIO = new EntityIO<Customer>();
+            var employeeIO = new EntityIO<Employee>();
+            var orderIO = new EntityIO<Order>();
+            var productIO = new EntityIO<Product>();
+
+            customerIO.Clear();
+            employeeIO.Clear();
+            orderIO.Clear();
+            productIO.Clear();
+
+            // Add new employee.
+
+            var employee = new Employee { Name = "Ion", Salary = 300 };
+            employeeIO.Insert(employee);
+            Console.WriteLine(employeeIO.Count().ToString());
+
+            // Tie new customer to the previous employee.
+            //customerIO.Insert(new Customer { Name = "Ion", Phone = "123", Employee = employee });
+            Console.WriteLine(customerIO.Count().ToString());
+            var query = from emp in employeeIO.Dbset
+                        orderby emp.Name
+                        select emp;
+            var employees = employeeIO.Retrieve(query);
+            foreach (var e in employees)
+            {
+                Console.WriteLine(String.Format("{0} has salary: {1} euro",
+                    e.Name, e.Salary.ToString()));
+            }
+        }
+    }
+}
