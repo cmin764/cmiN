b1ff98bf008c9f872eda3db0b6f12b9d22608c33
Cosmin Poieana <cmin764@gmail.com>
2017-05-30 20:10:54 +0300

    [FII] CN homework 6

--- FII/L3/CN/6/tema.py
+++ FII/L3/CN/6/tema.py
@@ -24,13 +24,53 @@ def save_random_matrix(dim):
     gen = lambda: random.randint(1, dim - 1)
     with open(FILE_A, "w") as stream:
         stream.write("{}\n\n".format(dim))
+        saved = {}
         while nr > 0:
             # Alegem o valoare, linie si coloana aleatoare.
-            value, lin, col = gen(), gen(), gen()
+            while True:
+                value, lin, col = gen(), gen(), gen()
+                pair = (lin, col)
+                if pair not in saved:
+                    saved[pair] = True
+                    break
             stream.write("{}, {}, {}\n".format(value, lin, col))
             # Punem si valoarea simetrica.
             stream.write("{}, {}, {}\n".format(value, col, lin))
             nr -= 1
+
+
+def norm_2(vec):
+    total = float(sum(elem ** 2 for elem in vec))
+    return total ** (1.0/2)
+
+
+def exponent_method(mat):
+    dim = mat.dim
+    # Generam vector aleator.
+    vec = [float(random.randint(1, dim)) for _ in range(dim)]
+    norm = norm_2(vec)
+    vec = [elem / norm for elem in vec]
+    assert sparse.is_zero(norm_2(vec) - 1.0 )
+    # Totul ok, incepem iteratiile de convergenta.
+    wvec = sparse.mat_vec_mul(mat, vec)
+    get_lmb = lambda vec1, vec2: sum([item[0] * item[1] for item in zip(vec1, vec2)])
+    lmb = get_lmb(wvec, vec)
+    count = 0
+    last_lmb = lmb
+    last_vec = vec
+    while True:
+        count += 1
+        aux = 1.0 / norm_2(wvec)
+        vec = [aux * elem for elem in wvec]
+        wvec = sparse.mat_vec_mul(mat, vec)
+        lmb = get_lmb(wvec, vec)
+        rez = [-last_lmb * elem for elem in last_vec]
+        rez = [sum(item) for item in zip(wvec, rez)]
+        if norm_2(rez) < dim * EPS or count > K_MAX:
+            break
+        last_lmb = lmb
+        last_vec = vec
+    return lmb, vec, count
 
 
 def main():
@@ -54,15 +94,23 @@ def main():
                                       trans=True)
         # Verificam daca matricea A, respectiv X este simetrica.
         pairs = [
-            (amat, tamat),
-            (xmat, txmat),
+            (amat, tamat, "A"),
+            (xmat, txmat, "X"),
         ]
-        for mat, tmat in pairs:
+        for mat, tmat, name in pairs:
             status = sparse.mat_equal(mat, tmat)
             if status:
                 logging.info("%r este matrice simetrica.", mat)
             else:
                 logging.warning("%r NU este matrice simetrica!", tmat)
+            # Obtinem valoarea si vectorul propriu.
+            val, vec, count = exponent_method(mat)
+            print("Valoare {}: {}".format(name, val))
+            print("Vector {}: {}".format(name, vec))
+            print("Iteratii {}: {}".format(name, count))
+            first = sparse.mat_vec_mul(mat, vec)
+            second = [val * elem for elem in vec]
+            assert first == second
 
 
 if __name__ == "__main__":
