443d3885e3d928408b92985e9dae96dec794ecf5
Cosmin Poieana <cmin764@gmail.com>
2017-10-03 22:45:14 +0300

    [FII] update LFAC & CSSO

---
+++ FII/L2/LFAC/scripts/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a, b, c;
+    a = 1;
+    b = 1;
+    c = 0;
+    c = a+++b;
+    printf("a: %d\n", a);
+    printf("b: %d\n", b);
+    printf("c: %d\n", c);
+    return 0;
+}
