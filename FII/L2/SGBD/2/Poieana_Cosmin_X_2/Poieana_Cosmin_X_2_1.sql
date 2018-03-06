32d7e22b2fbb89ef3d5b5934d20e4092905a9b2d
Cosmin Poieana <cmin764@gmail.com>
2018-03-07 00:02:47 +0200

    [FII] SGBD homework update

--- FII/L2/SGBD/2/Poieana_Cosmin_X_2/Poieana_Cosmin_X_2_1.sql
+++ FII/L2/SGBD/2/Poieana_Cosmin_X_2/Poieana_Cosmin_X_2_1.sql
@@ -24,9 +24,8 @@ DECLARE
     -- cifra de control pt. verificare
     control_nr CONSTANT NUMBER := 7;
     -- numar intrari in tabel
-    items_count CONSTANT NUMBER := 100;
+    items_count CONSTANT NUMBER := 10000;
     
-    gen_NR NUMBER;
     aux NUMBER;
     digit NUMBER;
     total NUMBER;
@@ -35,51 +34,58 @@ DECLARE
     root NUMBER;
 BEGIN
 
-    FOR idx IN 1..items_count LOOP
-        DBMS_OUTPUT.PUT_LINE('Intrarea ' || idx);
-        -- generam numar aleator mai cu o cifra mai putin
-        gen_nr := TRUNC(DBMS_RANDOM.VALUE(1, 1000));
+    FOR gen_nr IN 1..items_count LOOP
+--        DBMS_OUTPUT.PUT_LINE('Intrarea ' || idx);
+        -- generam numar aleator cu o cifra mai putin
+--        gen_nr := TRUNC(DBMS_RANDOM.VALUE(1, 1000));
         -- clona a lui
         aux := gen_nr;
         -- calculam suma cifrelor
         total := 0;
-        WHILE (gen_nr > 0) LOOP
-            digit := gen_nr MOD 10;
+        WHILE (aux > 0) LOOP
+            digit := aux MOD 10;
             total := total + digit;
-            gen_nr := TRUNC(gen_nr / 10);
+            aux := TRUNC(aux / 10);
         END LOOP;
         
         -- ajustam ultima cifra in functie de ce am obtinut pana acum
         total := total MOD 9;
-        digit := control_nr - total;
-        -- normalizare valoare negativa
-        IF (digit < 0) THEN
-            digit := 9 + digit;
+        IF (total <> control_nr) THEN
+            CONTINUE;
         END IF;
+--        digit := control_nr - total;
+        -- normalizare valoare negativa
+--        IF (digit < 0) THEN
+--            digit := 9 + digit;
+--        END IF;
         
-        aux := aux * 10 + digit;
-        DBMS_OUTPUT.PUT_LINE('Numar generat: ' || aux);
+--        aux := aux * 10 + digit;
+--        DBMS_OUTPUT.PUT_LINE('Numar generat: ' || aux);
         
         -- verificare primalitate
         is_prime := 1;
-        IF (aux MOD 2 = 0) THEN
+        IF (gen_nr < 2) THEN
+            is_prime := 0;
+        ELSIF (gen_nr = 2) THEN
+            is_prime := 1;
+        ELSIF (gen_nr MOD 2 = 0) THEN
             is_prime := 0;
         ELSE
-            root := CEIL(SQRT(aux));
+            root := CEIL(SQRT(gen_nr));
 --            DBMS_OUTPUT.PUT_LINE('Root: ' || root);
             FOR nr IN 3..root LOOP
                 IF (nr MOD 2 = 0) THEN
                     CONTINUE;
                 END IF;
-                IF (aux MOD nr = 0) THEN
+                IF (gen_nr MOD nr = 0) THEN
                     is_prime := 0;
                     EXIT;
                 END IF;
             END LOOP;
         END IF;
         
-        DBMS_OUTPUT.PUT_LINE('Prim: ' || is_prime);
-        INSERT INTO Primes VALUES (aux, is_prime);
+--        DBMS_OUTPUT.PUT_LINE('Prim: ' || is_prime);
+        INSERT INTO Primes VALUES (gen_nr, is_prime);
         
     END LOOP;
     
