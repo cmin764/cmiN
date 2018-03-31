6c552d2a231bf3f57ff0af1037e80f9e6cde21a1
Cosmin Poieana <cmin764@gmail.com>
2018-03-31 17:31:59 +0300

    [FII] SGBD homework 5

---
+++ FII/L2/SGBD/5/Poieana_Cosmin_X_5/Poieana_Cosmin_X_5_1.sql
@@ -0,0 +1,105 @@
+--DROP TABLE Erasmus;
+--/
+--
+--CREATE TABLE Erasmus (
+--    nr_matricol VARCHAR2(6) NOT NULL,
+--    nume VARCHAR2(15) NOT NULL,
+--    prenume VARCHAR2(30) NOT NULL,
+--    tara VARCHAR2(2)
+--);
+--/
+--
+--
+--CREATE UNIQUE INDEX erasmus_matricol_index ON Erasmus(nr_matricol);
+--/
+
+CREATE OR REPLACE FUNCTION rand(start_int NUMBER, end_int NUMBER)
+RETURN NUMBER AS
+    ret NUMBER;
+BEGIN
+    ret := TRUNC(DBMS_RANDOM.VALUE(start_int, end_int + 1));
+    RETURN ret;
+END;
+/
+
+
+CREATE OR REPLACE PACKAGE Exceptii AS
+    throw CONSTANT BOOLEAN := FALSE;
+    
+    student_duplicat EXCEPTION;
+    bursa_depasita EXCEPTION;
+    
+    PRAGMA EXCEPTION_INIT(student_duplicat, -20001);
+    PRAGMA EXCEPTION_INIT(bursa_depasita, -20002);
+END Exceptii;
+/
+
+
+CREATE OR REPLACE PACKAGE Erasmus_pack AS
+    NMAX CONSTANT NUMBER := 100;
+    MMAX CONSTANT NUMBER := 10;
+    
+    PROCEDURE erasmus_populeaza;
+END Erasmus_pack;
+/
+
+
+CREATE OR REPLACE PACKAGE BODY Erasmus_pack AS
+
+    PROCEDURE erasmus_populeaza AS
+        stud_id Studenti.id%TYPE;
+        stud_line Studenti%ROWTYPE;
+        count_stud NUMBER;
+    BEGIN
+        FOR idx IN 1..NMAX LOOP
+            stud_id := rand(1, 1024);
+            SELECT * INTO stud_line
+                FROM Studenti
+                WHERE Studenti.id = stud_id;
+            DBMS_OUTPUT.PUT_LINE('[*] Student ales: ' || stud_line.nume || ' ' ||
+                                 stud_line.prenume || ' (' || stud_line.id || ')');
+                                 
+            DECLARE
+            BEGIN
+                SELECT COUNT(1) INTO count_stud
+                    FROM Erasmus
+                    WHERE Erasmus.nr_matricol = stud_line.nr_matricol;
+                IF (count_stud > 0) THEN
+                    RAISE Exceptii.student_duplicat;
+                END IF;
+                
+                INSERT INTO Erasmus VALUES (
+                    stud_line.nr_matricol,
+                    stud_line.nume,
+                    stud_line.prenume,
+                    'is'
+                );
+                COMMIT;
+                DBMS_OUTPUT.PUT_LINE('[i] Adaugat cu succes: ' || stud_id);
+            EXCEPTION
+                WHEN Exceptii.student_duplicat THEN
+                    IF (Exceptii.throw) THEN
+                        raise_application_error(-20001, '[x] Student duplicat.');
+                    ELSE
+                        DBMS_OUTPUT.PUT_LINE('[x] Student erasmus duplicat.');
+                    END IF;
+            END;
+            
+        END LOOP;
+    END;
+END Erasmus_pack;
+/
+
+
+SET serveroutput ON;
+
+DECLARE
+BEGIN
+    -- Populeaza tabela Erasmus cu N(100) studenti bursieri in tari.
+    Erasmus_pack.erasmus_populeaza();
+
+    DBMS_OUTPUT.PUT_LINE('End.');
+EXCEPTION
+    WHEN Exceptii.student_duplicat THEN
+        DBMS_OUTPUT.PUT_LINE('[x] Student duplicat.');
+END;
---
+++ FII/L2/SGBD/5/Poieana_Cosmin_X_5/Poieana_Cosmin_X_5_2.sql
@@ -0,0 +1,117 @@
+CREATE OR REPLACE PACKAGE Majorare_pack AS
+    NMAX CONSTANT NUMBER := 100;
+    MMAX CONSTANT NUMBER := 10;
+    
+    PROCEDURE majoreaza_bursa(stud_id Studenti.id%TYPE);
+    PROCEDURE top_burse;
+END Majorare_pack;
+/
+
+
+CREATE OR REPLACE PACKAGE BODY Majorare_pack AS
+    
+    PROCEDURE majoreaza_bursa(stud_id Studenti.id%TYPE) AS
+        procent_implicit CONSTANT FLOAT := 10;
+        max_bursa CONSTANT Studenti.bursa%TYPE := 3000;
+        
+        mariri Bursa.lista_mariri;
+        m1 Bursa.marire;
+        
+        bursa_actuala Studenti.bursa%TYPE;
+    BEGIN
+        m1.stud_id := stud_id;
+        m1.procent := procent_implicit;
+        mariri := Bursa.lista_mariri(m1);
+        
+        Bursa.mareste_bursa(mariri);
+        
+        -- Verifica conditiile bursei.
+        SELECT Studenti.bursa INTO bursa_actuala
+            FROM Studenti
+            WHERE Studenti.id = stud_id;
+        IF (bursa_actuala > max_bursa) THEN
+            -- Trunchiaza bursa.
+            UPDATE Studenti
+                SET Studenti.bursa = max_bursa
+                WHERE Studenti.id = stud_id;
+            COMMIT;
+            
+            -- Acum arunca eroare cu privire la asta.
+            RAISE Exceptii.bursa_depasita;
+        END IF;
+    END;
+    
+    FUNCTION modificat(stud_id Studenti.id%TYPE)
+    RETURN BOOLEAN AS
+        status BOOLEAN;
+    BEGIN
+        status := (Bursa.modificati.EXISTS(stud_id) AND
+                   Bursa.modificati(stud_id) = TRUE);
+        RETURN status;
+    END;
+    
+    PROCEDURE top_burse AS
+        CURSOR studenti_crs IS
+            SELECT * FROM Studenti
+            WHERE Studenti.istoric_bursa IS NOT NULL
+            ORDER BY Studenti.bursa DESC;
+        studenti_line studenti_crs%ROWTYPE;
+        
+        total NUMBER := 1;
+        
+        delta Studenti.bursa%TYPE;
+    BEGIN
+        OPEN studenti_crs;
+        LOOP
+            FETCH studenti_crs INTO studenti_line;
+            EXIT WHEN studenti_crs%NOTFOUND;
+            
+            IF (modificat(studenti_line.id) = FALSE) THEN
+                CONTINUE;
+            END IF;
+            
+            -- Afisam si numerotam studentul.
+            DBMS_OUTPUT.PUT_LINE('[i] ' || total || '. ' || studenti_line.nume || ' ' ||
+                studenti_line.prenume || ' (' || studenti_line.id || ').');
+            delta := studenti_line.bursa - studenti_line.istoric_bursa(studenti_line.istoric_bursa.LAST);
+            DBMS_OUTPUT.PUT_LINE('    Bursa noua: ' || studenti_line.bursa || ' (' ||
+                 '+' || delta || ').');
+            
+            total := total + 1;
+            IF (total > Majorare_pack.MMAX) THEN
+                EXIT;
+            END IF;
+        END LOOP;
+        CLOSE studenti_crs;
+    END;
+END Majorare_pack;
+/
+
+
+SET serveroutput ON;
+
+DECLARE
+    stud_id Studenti.id%TYPE;
+BEGIN
+    -- Curata date anterioare in legatura cu studentii carora li s-a modificat
+    -- bursa.
+    Bursa.modificati.DELETE;
+    COMMIT;
+    
+    -- Majoreaza bursa la N(100) studenti si trateaza exceptia aruncata in acest bloc.
+    FOR idx in 1..Majorare_pack.NMAX LOOP
+        stud_id := rand(1, 1024);
+        Majorare_pack.majoreaza_bursa(stud_id);
+    END LOOP;
+    -- Acum afiseaza doar pe cei carora tocmai li s-a facut marirea in
+    -- aceasta sesiune (no toti dintotdeauna).
+--    Bursa.afiseaza_modificari(toti => FALSE);
+    
+    -- Afisam primii M(10) studenti fruntasi si bursele lor (+ majorare).
+    Majorare_pack.top_burse();
+
+    DBMS_OUTPUT.PUT_LINE('End.');
+EXCEPTION
+    WHEN Exceptii.bursa_depasita THEN
+        DBMS_OUTPUT.PUT_LINE('[x] A fost depasita bursa maxima.');
+END;
